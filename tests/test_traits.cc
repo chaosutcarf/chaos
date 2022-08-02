7c4ffb6790ad8271941565937a9c1e99679b8ef0
LamKamhang <lamkamh@zju.edu.cn>
2022-08-02 22:37:30 +0800

    fix compile

--- tests/test_traits.cc
+++ tests/test_traits.cc
@@ -1,12 +1,13 @@
 #include "iostream"
-#include "mapping/concept.hh"
+#include "mapping/filler_concepts.hh"
 using namespace chaos::mapping;
 using namespace std;
 
 struct A {
   static constexpr bool Override{true};
   static constexpr bool CanGetData{true};
-  static constexpr chaos::MATRIX_FILL_MODE MatFillMode{chaos::MATRIX_FILL_MODE::FULL};
+  static constexpr chaos::MATRIX_FILL_MODE MatFillMode{
+      chaos::MATRIX_FILL_MODE::FULL};
   using Traits = FillerTraits<A>;
 
   template <bool fillmode = Traits::Override()>
